d7dc00637efe37005985f52280e4ae1a28d79ddf
t0anh <anh95.nt@gmail.com>
2024-10-23 11:10:07 +0700

    Update flag google login

--- packages/gas-station/config/vars.ts
+++ packages/gas-station/config/vars.ts
@@ -1,3 +1,5 @@
+const { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } = process.env
+
 export const enableLoginWithEmail = false
 export const enableLoginWithGitHub = true
-export const enableLoginWithGoogle = true
+export const enableLoginWithGoogle = !!AUTH_GOOGLE_ID && !!AUTH_GOOGLE_SECRET
